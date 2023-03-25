8a280ea8d25f9d7958c7b3dca603cfc7cd57a0df
newkayak12 <newkayak12@gmail.com>
2023-03-25 10:37:23 +0900

    swift

---
+++ 0.Welcome To Swift/04.A Swift Tour.playground/Pages/01.SimpleValues.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+
+import Foundation
+
+/*:# Simple Value*/
+
+/**
+ Use 'let' to make a constant and 'var' make a variable. The value of a constant dosen't need to be knwon at compile time, but you must assign it a vlaue exactly once.
+ 
+*/
+
+
+var myVairable = 42
+myVairable = 12
+let myConstant = 123
+
+
+
+
+
+
+
+//: [Next](@next)
